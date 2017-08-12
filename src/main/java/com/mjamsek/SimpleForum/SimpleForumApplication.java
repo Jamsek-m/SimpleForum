fd09f91c6e40085aad92616455145bcc650ea929
Miha Jamsek <miha_jamsek@windowslive.com>
2017-08-12 14:09:42 +0200

    init

---
+++ src/main/java/com/mjamsek/SimpleForum/SimpleForumApplication.java
@@ -0,0 +1,12 @@
+package com.mjamsek.SimpleForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleForumApplication.class, args);
+	}
+}
